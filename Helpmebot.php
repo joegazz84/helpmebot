deec3c5c8f48544c8779bf9a218e02a8d8dcbef4
Simon Walker <github@helpmebot.org.uk>
2010-08-12 22:18:30 +0000

    Add messages special page

--- Helpmebot.php
+++ Helpmebot.php
@@ -9,6 +9,8 @@ if(!defined('MEDIAWIKI')) {
 
 define('NS_COMMAND', 204);
 define('NS_COMMAND_TALK', 205);
+define('NS_MESSAGE', 206);
+define('NS_MESSAGE_TALK', 207);
 
 $wgExtensionCredits['specialpage'][] = array(
 'path' => __FILE__,
@@ -19,23 +21,31 @@ $wgExtensionCredits['specialpage'][] = array(
 
 $wgExtraNamespaces[NS_COMMAND] = "Command";
 $wgExtraNamespaces[NS_COMMAND_TALK] = "Command_talk:";
+$wgExtraNamespaces[NS_MESSAGE] = "Message";
+$wgExtraNamespaces[NS_MESSAGE_TALK] = "Message_talk:";
 
 SpecialVersion::$viewvcUrls['http://svn.helpmebot.org.uk/svn/helpmebot'] = 'http://svn.helpmebot.org.uk/viewvc/helpmebot';
 
 $wgAutoloadClasses['SpecialAccessList'] = $IP . '/extensions/Helpmebot/SpecialAccessList.php';
 $wgAutoloadClasses['AccessListPager'] = $IP . '/extensions/Helpmebot/AccessListPager.php';
+
 $wgAutoloadClasses['SpecialBrain'] = $IP.'/extensions/Helpmebot/SpecialBrain.php';
 $wgAutoloadClasses['BrainPager'] = $IP.'/extensions/Helpmebot/BrainPager.php';
+
 $wgAutoloadClasses['CommandListPager'] = $IP.    '/extensions/Helpmebot/CommandListPager.php';
 $wgAutoloadClasses['SpecialCommandList'] = $IP . '/extensions/Helpmebot/SpecialCommandList.php';
+
+$wgAutoloadClasses['SpecialBotMessages'] = $IP . '/extensions/Helpmebot/SpecialBotMessages.php';
 
 $wgSpecialPages['AccessList'] = 'SpecialAccessList';
 $wgSpecialPages['Brain'] = 'SpecialBrain';
 $wgSpecialPages['CommandList'] = 'SpecialCommandList';
+$wgSpecialPages['BotMessages'] = 'SpecialBotMessages';
 
 $wgSpecialPageGroups['AccessList'] = 'helpmebot';
 $wgSpecialPageGroups['Brain']="helpmebot";
 $wgSpecialPageGroups['CommandList']="helpmebot";
+$wgSpecialPageGroups['BotMessages']="helpmebot";
 
 $wgExtensionMessagesFiles['Helpmebot'] = $IP . '/extensions/Helpmebot/Helpmebot.i18n.php';
 
