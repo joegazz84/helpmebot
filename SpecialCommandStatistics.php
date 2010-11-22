eb1a951b9b27f6d23b46bfee701cf42e9fcff2ec
Simon Walker <github@helpmebot.org.uk>
2010-11-22 05:47:22 +0000

    fix

--- SpecialCommandStatistics.php
+++ SpecialCommandStatistics.php
@@ -15,7 +15,7 @@ class SpecialCommandStatistics extends SpecialPage {
       google.setOnLoadCallback(drawChart);
       function drawChart() {
         //Tell Google Visualization where your script is
-        var query = new google.visualization.Query('/w/extensions/Helpmebot/vis.php');
+        var query = new google.visualization.Query('/w/extensions/Helpmebot/viz.php');
         query.setQuery('select al_class, count(*) from hmb_accesslog group by al_class');
         query.send(function(result) {
           if(result.isError()) {
